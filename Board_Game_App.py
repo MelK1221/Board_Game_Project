e14bc253d8c2d1e7f51bdb471da363da01dd7c4a
Melanie Kukura <melaniekukura@Melanies-MacBook-Air.local>
2025-04-10 18:37:34 -0400

    Creating Python entry point file

---
+++ Board_Game_App.py
@@ -0,0 +1,9 @@
+# Entry point python file to start Board Game Project
+# Emily Kukura and Melanie Kukura
+
+fav_board_games = ["Codenames", "Hanabi", "Mysterium", "Settlers of Catan"]
+
+print("The following are a few of our favorite board games, in no particular order:")
+
+for game in range(len(fav_board_games)):
+    print(fav_board_games[game])
