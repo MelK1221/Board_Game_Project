013c5ec4cac18966cdbcabaa72ba91b04e41a500
ekukura <kukuraemily@gmail.com>
2025-04-17 21:25:31 -0600

    Initial FastAPI App

--- Board_Game_App.py
+++ Board_Game_App.py
@@ -1,27 +1,46 @@
+#!/usr/bin/env python3
 """
 Entry point python file to start Board Game Project.
 
 Authors: Emily Vaughn-Kukura and Melanie Kukura
 """
-
 import argparse
 import csv
 import json
 import os
 
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Board Games API",
+    summary="Kukura Family & Friends Board Game Ratings",
+    version="1",
+    servers=[{"url": "/"}],
+)
+
+
+@app.get("/api/players/")
+def get_players():
+    return app.games_by_player
+
+
 ALL="all"
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
 
     # Add arguments
-    parser.add_argument("-p","--player", default= ALL, help="Name of Player")
-    parser.add_argument("-f","--file",  default= "example_files/fam_fav_games.json", help="Player favorite games file")
+    parser.add_argument("--port", default=8080, help="Port number to run the server on")
+    parser.add_argument("-p","--player", help="Player name to get favorite games for")
+    parser.add_argument("-f","--file",  default="example_files/fam_fav_games.json", help="Player favorite games file")
     parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
 
     # Parse arguments
     args = parser.parse_args()
     return args
+
 
 def parse_players_file(filename, ext) -> dict[str, list]:
     """
@@ -54,21 +73,11 @@ def parse_players_file(filename, ext) -> dict[str, list]:
         return player_games_dict
 
 
-def run(args: argparse.Namespace):
-    games_by_player = {}
-
-    # Check for existence of path
-    if os.path.exists(args.file):
-        name, extension = os.path.splitext(args.file)
-        games_by_player = parse_players_file(args.file, extension)
-    else:
-        raise FileNotFoundError(f"File not found: {args.file}")
-    
-    # Check if players exist in dict
-    if not games_by_player:
-        msg = "No players found in provided file."
-        raise ValueError(msg)
-
+def print_player_likes(args: argparse.Namespace, games_by_player: dict[str, list]):
+    """
+    Print the players and their favorite games.
+    If verbose is set, print the games that all players like.
+    """
     # Print requested player(s) and their favorite games    
     players: list = []
     if args.player == ALL:
@@ -80,7 +89,6 @@ def run(args: argparse.Namespace):
             msg = f"{req_player} is not in the loaded file."
             raise ValueError(msg)
         players = [req_player]
-
 
     print("The following is a list of current players and their favorite games:")
     for player in players:
@@ -107,6 +115,27 @@ def run(args: argparse.Namespace):
         else:
             print(f"All players like: {', '.join(list(joint_likes))}")
 
+
+def run(args: argparse.Namespace):
+    games_by_player = {}
+
+    # Check for existence of path
+    if os.path.exists(args.file):
+        _, extension = os.path.splitext(args.file)
+        games_by_player = parse_players_file(args.file, extension)
+    else:
+        raise FileNotFoundError(f"File not found: {args.file}")
+    
+    # Check if players exist in dict
+    if not games_by_player:
+        msg = "No players found in provided file."
+        raise ValueError(msg)
+
+    if args.player:
+        print_player_likes(args, games_by_player)
+    else:
+        app.games_by_player = games_by_player
+        uvicorn.run(app, host="localhost", port=args.port)
 
 if __name__ == "__main__":
     args = parse_args()
