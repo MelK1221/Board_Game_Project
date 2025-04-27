bfeccd9e21d3bd418d5448b0618cd9590eaa6460
ekukura <kukuraemily@gmail.com>
2025-04-26 21:53:36 -0600

    Add basic test case and update README with instructions for running tests

--- test_board_game_app.py
+++ test_board_game_app.py
@@ -1,9 +1,40 @@
+import pytest
+from Board_Game_App import find_games_list, find_player_idx
 
-def func(x):
-    return x + 1
+em_games = [
+    {"Game": "Boggle", "Rating": 7},
+    {"Game": "Hanabi", "Rating": 6},
+]
 
+mel_games = [
+    {"Game": "Codenames", "Rating": 8},
+    {"Game": "Hanabi", "Rating": 8},
+    {"Game": "Mysterium", "Rating": 7},
+]
 
-# pytest will run all files of the form test_*.py or *_test.py in the current directory and its subdirectories.
-def test_answer():
-    assert func(4) == 5
+games_data = [
+   {
+      "Name": "Em",
+      "Games": em_games,
+   },
+   {
+      "Name": "Mel",
+      "Games": mel_games
+   },
+]
 
+def test_find_player_idx():
+    res = find_player_idx(games_by_player=games_data, key="Name", value="Em")
+    assert res == 0
+
+def test_find_player_idx_player_not_present():
+    res = find_player_idx(games_by_player=games_data, key="Name", value="Bad")
+    assert res == -1
+
+def test_find_games_list():
+    res = find_games_list(games_by_player=games_data, player_idx=0)
+    assert res == ["Boggle", "Hanabi"]
+
+def test_find_games_list_invalid_index():
+    with pytest.raises(IndexError):
+        find_games_list(games_by_player=games_data, player_idx=2)
