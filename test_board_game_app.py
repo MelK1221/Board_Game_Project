069d63d3324cdaceb8315be4b603d9f7cc08421f
MelK1221 <mlkukura1221@gmail.com>
2025-07-21 17:25:14 -0400

    Add class setup/teardown

--- test_board_game_app.py
+++ test_board_game_app.py
@@ -3,43 +3,57 @@ from fastapi.testclient import TestClient
 from Board_Game_App import create_games_by_player, all_games, app
 
 ### Setup Test Data ###
-em_games = {
-         "Boggle": 7,
-         "Hanabi": 6,
-         "Mysterium": 9,
-         "Rivals of Catan": 8
-      }
 
-mel_games = {
-         "Codenames": 8,
-         "Hanabi": 8,
-         "Mysterium": 7,
-         "Settlers of Catan": 6
-      }
+def sample_data_setup():
+    """
+    Setup sample data to use in tests
+    """
+    em_games = {
+            "Boggle": 7,
+            "Hanabi": 6,
+            "Mysterium": 9,
+            "Rivals of Catan": 8
+        }
 
-games_data = [
-    {
-        "Name": "Em",
-        "Games": em_games
-    },
-    {
-        "Name": "Mel",
-        "Games": mel_games
-    }
-]
+    mel_games = {
+            "Codenames": 8,
+            "Hanabi": 8,
+            "Mysterium": 7,
+            "Settlers of Catan": 6
+        }
 
-client = TestClient(app)
+    games_data = [
+        {
+            "Name": "Em",
+            "Games": em_games
+        },
+        {
+            "Name": "Mel",
+            "Games": mel_games
+        }
+    ]
+    games_by_player = create_games_by_player(games_data)
+    all_player_games = all_games(games_by_player)
 
-games_by_player = create_games_by_player(games_data)
-app.games_by_player = games_by_player
+    return games_data, games_by_player, all_player_games
 
-all_player_games = all_games(games_by_player)
-app.all_player_games = all_player_games
+def start_application():
+    games_data, games_by_player, all_player_games = sample_data_setup()
+    client = TestClient(app)
+    app.games_by_player = games_by_player
+    app.all_player_games = all_player_games
+
+    return client
+
 
 ### Test Supporting Funtions ###
 class Test_Supporting_Funcs:
+
+    def setup_method(self, method):
+        self.games_data, self.games_by_player, self.all_player_games = sample_data_setup()
+
     def test_create_games_by_player(self):
-        res = create_games_by_player(players_games_list=games_data)
+        res = create_games_by_player(players_games_list=self.games_data)
         assert res == {
             "Em": {
                 "Boggle": 7,
@@ -56,7 +70,7 @@ class Test_Supporting_Funcs:
             }
         
     def test_all_games(self):
-        games = create_games_by_player(games_data)
+        games = create_games_by_player(self.games_data)
         res = all_games(games_by_player = games)
         expected_list = ["Boggle", "Hanabi", "Mysterium", "Rivals of Catan", "Codenames", "Settlers of Catan"]
         assert sorted(res) == sorted(expected_list)
@@ -64,13 +78,23 @@ class Test_Supporting_Funcs:
 
 ### Test API Get Endpoints ###
 class Test_API_Players_Path:
+    
+    @classmethod
+    def setup_class(cls):
+        cls.client = start_application()
+        cls.games_data, cls.games_by_player, cls.all_player_games = sample_data_setup()
+    
+    @classmethod
+    def teardown_class(cls):
+        cls.client.close()
+
     def test_get_players(self):
-        response = client.get("/api/players")
+        response = self.client.get("/api/players")
         assert response.status_code == 200
-        assert response.json() == games_by_player
+        assert response.json() == self.games_by_player
 
     def test_get_player(self):
-        response = client.get("/api/players/em")
+        response = self.client.get("/api/players/em")
         assert response.status_code == 200
         assert response.json() == {
             "Boggle": 7,
@@ -80,18 +104,28 @@ class Test_API_Players_Path:
             }
         
     def test_get_player_invalid_name(self):
-        response = client.get("/api/players/bad")
+        response = self.client.get("/api/players/bad")
         assert response.status_code == 404
         assert response.json() == {"detail": "Player Bad not found."}
 
 class Test_API_Games_Path():
+    
+    @classmethod
+    def setup_class(cls):
+        cls.client = start_application()
+        cls.games_data, cls.games_by_player, cls.all_player_games = sample_data_setup()
+    
+    @classmethod
+    def teardown_class(cls):
+        cls.client.close()
+    
     def test_get_games(self):
-        response = client.get("/api/games")
+        response = self.client.get("/api/games")
         assert response.status_code == 200
-        assert response.json() == all_player_games
+        assert response.json() == self.all_player_games
 
     def test_get_game_ratings(self):
-        response = client.get("/api/games/hanabi")
+        response = self.client.get("/api/games/hanabi")
         assert response.status_code == 200
         assert response.json() == {
             "Em": 6,
@@ -99,22 +133,22 @@ class Test_API_Games_Path():
         }
 
     def test_get_game_ratings_invalid_game(self):
-        response = client.get("/api/games/zelda")
+        response = self.client.get("/api/games/zelda")
         assert response.status_code == 404
         assert response.json() == {"detail": "Game Zelda not found."}
 
     def test_get_player_rating(self):
-        response = client.get("/api/games/hanabi/mel")
+        response = self.client.get("/api/games/hanabi/mel")
         assert response.status_code == 200
         assert response.json() == 8
 
     def test_get_player_rating_invalid_name(self):
-        response = client.get("/api/games/hanabi/bad")
+        response = self.client.get("/api/games/hanabi/bad")
         assert response.status_code == 404
         assert response.json() == {"detail": "Player Bad not found."}
 
     def test_get_player_rating_invalid_game(self):
-        response = client.get("/api/games/zelda/em")
+        response = self.client.get("/api/games/zelda/em")
         assert response.status_code == 404
         assert response.json() == {"detail": "Game Zelda not rated by Em."}
   
